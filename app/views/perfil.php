ec36d5e7aff6c62f55f32c8156e524ab6b6e6e22
gusta1996 <gustasama1996@gmail.com>
2023-11-30 22:44:28 -0500

    modified:   app/views/perfil.php

--- app/views/perfil.php
+++ app/views/perfil.php
@@ -5,6 +5,7 @@ require '../models/user_session.php';
 $userSession = new UserSession();
 $user = new User();
 
+
 if ($userSession->userLoggedIn()) {
     // Hay sesión
     $user->setUser($userSession->getCurrentUser());
