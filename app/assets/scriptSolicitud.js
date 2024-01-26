7a411af2d56d6ed853e85de60c7f279fdb1c8db9
gusta1996 <gustasama1996@gmail.com>
2024-01-26 14:03:36 -0500

    rol validador implementado

--- app/assets/scriptSolicitud.js
+++ app/assets/scriptSolicitud.js
@@ -81,7 +81,7 @@ const appSolicitud = new (function () {
                                     </button>
                                 ` : `` }
 
-                                ${ ( this.tablaAdministrador || this.tablaValidador || this.tablaAutorizador ) && ( item[i].fs_estado == 'pendiente' && item[i].estado_autorizador == 'pendiente' && item[i].estado_validador == 'pendiente' ) ? `
+                                ${ ( this.tablaAdministrador ) && ( item[i].fs_estado == 'pendiente' && item[i].estado_autorizador == 'pendiente' && item[i].estado_validador == 'pendiente' ) ? `
                                     <!-- Boton aprobar con validador BLOQUEADO -->
                                     <button onclick="" title="Esta solicitud necesita ser aprobada por un validador" class="btn-aprobar cursor-no-drop flex items-center gap-2 min-h-fit rounded-md bg-gray-50 px-3 py-2 text-xs font-medium text-gray-700 ring-1 ring-inset ring-gray-700/10">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check-fill" viewBox="0 0 16 16">
@@ -90,9 +90,18 @@ const appSolicitud = new (function () {
                                         </svg>
                                     </button>
                                 ` : `` }
-                                ${ ( this.tablaAutorizador ) && ( item[i].fs_estado == 'pendiente' && item[i].estado_autorizador == 'aprobado' && item[i].estado_validador == 'pendiente' ) ? `
+                                ${ ( this.tablaAutorizador ) && ( item[i].fs_estado == 'pendiente' && item[i].estado_validador == 'pendiente' ) ? `
                                     <!-- Boton aprobar con validador BLOQUEADO -->
-                                    <button onclick="" title="Espere a que un validador apruebe esta solicitud" class="btn-aprobar cursor-no-drop flex items-center gap-2 min-h-fit rounded-md bg-gray-50 px-3 py-2 text-xs font-medium text-gray-700 ring-1 ring-inset ring-gray-700/10">
+                                    <button onclick="" title="Esta solicitud necesita ser aprobada por un validador" class="btn-aprobar cursor-no-drop flex items-center gap-2 min-h-fit rounded-md bg-gray-50 px-3 py-2 text-xs font-medium text-gray-700 ring-1 ring-inset ring-gray-700/10">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check-fill" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
+                                            <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                                        </svg>
+                                    </button>
+                                ` : `` }
+                                ${ ( this.tablaValidador ) && ( item[i].fs_estado == 'pendiente' && item[i].estado_autorizador == 'pendiente' && item[i].estado_validador == 'pendiente' ) ? `
+                                    <!-- Boton aprobar con validador BLOQUEADO -->
+                                    <button onclick="" title="Esta solicitud necesita ser aprobada por un autorizador" class="btn-aprobar cursor-no-drop flex items-center gap-2 min-h-fit rounded-md bg-gray-50 px-3 py-2 text-xs font-medium text-gray-700 ring-1 ring-inset ring-gray-700/10">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-check-fill" viewBox="0 0 16 16">
                                             <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
                                             <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
